94d27bf86831b8e7e1f8ea22c285b9a872a243ce
maitreya29 <maitreyapatni30@gmail.com>
2021-10-08 01:06:47 +0200

    lavender: Build android.hardware.radio.deprecated
    F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/qcrild": library "android.hardware.radio.deprecated@1.0.so" not found
    
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>

--- device.mk
+++ device.mk
@@ -379,6 +379,8 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
+    android.hardware.radio.deprecated@1.0 \
+    android.hardware.radio.deprecated@1.0.vendor \
     android.hardware.secure_element@1.0 \
     librmnetctl \
     libprotobuf-cpp-full \

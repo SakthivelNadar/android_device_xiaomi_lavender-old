ba240b740010583b8f80cdd42da73fe0ea2022a6
Raiyan Bin Mohsin <raiyanbinmohsinshishir@gmail.com>
2021-10-14 00:00:44 +0600

    lavender: drop `keymaster 3.0`

--- device.mk
+++ device.mk
@@ -443,13 +443,8 @@ PRODUCT_PACKAGES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service \
     android.hardware.keymaster@4.0-impl \
     android.hardware.keymaster@4.0-service \
-    android.hardware.keymaster@3.0.vendor \
-    android.hardware.keymaster@4.0.vendor \
-    android.hardware.keymaster@4.1.vendor \
     android.hardware.gatekeeper@1.0 \
     android.hardware.gatekeeper@1.0.vendor
 
